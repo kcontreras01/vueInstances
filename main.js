59ac662fab5bac420947df1690a62cf006622054
Kiara Contreras <kiara@hashtag-labs.com>
2018-04-25 11:44:57 -0400

    methods

---
+++ main.js
@@ -0,0 +1,26 @@
+var vm = new Vue({
+  el: '#example',
+  data: {
+    message: 'Hello'
+  },
+  computed: {
+    // a computed getter
+    reversedMessage: function () {
+      // `this` points to the vm instance
+      return this.message.split('').reverse().join('')
+    }
+  }
+})
+
+var demo = new Vue({
+	el: "#demo",
+	data: {
+		firstName: 'Foo',
+		lastName: 'Bar',
+	},
+	computed: {
+		fullName: function(){
+			return this.firstName + ' ' + this.lastName
+		}
+	}
+})
